abfdeb85f9aafd23ba2fab90ba973967da2ee0aa
Aayan Kumar Singh <84129146+aayankr7@users.noreply.github.com>
2026-06-08 03:01:18 +0530

    completed parsing file

--- Parser/scavenger.cpp
+++ Parser/scavenger.cpp
@@ -39,33 +39,60 @@ int main()
         image.read(directoryHeader, 2);
         offsetDirectoryHeader = ((unsigned char)directoryHeader[1]<<8)|
                                 ((unsigned char)directoryHeader[0]);
-        j++;
-        if(j>1000)
-        {
-            cout<<"failed here";
-            return 1;
-        }
-    }
-    if(j<1000)
-    {
-        cout<<"found it";
     }
 
-    /*image.seekg(6, ios::cur);
-    image.read(offsetBytes,4);
+    image.seekg(6, ios::cur);
+    image.read(offsetBytes, 4);
     offset = ((unsigned char)offsetBytes[3]<<24)|
              ((unsigned char)offsetBytes[2]<<16)|
              ((unsigned char)offsetBytes[1]<<8)|
              ((unsigned char)offsetBytes[0]);
-    image.seekg(offset, ios::beg);
+    uint32_t SubIFD = offset;
+    image.seekg(offset+2, ios::beg);
 
-    image.read(test, 4);
-    for(int i = 0; i<4; i++)
+    image.read(directoryHeader, 2);
+    offsetDirectoryHeader = ((unsigned char)directoryHeader[1]<<8)|
+                            ((unsigned char)directoryHeader[0]);
+    
+    while(offsetDirectoryHeader!=0x0111)
     {
-        cout<<hex
-            <<setw(2)
-            <<setfill('0')
-            <<(unsigned int)(unsigned char)test[i]
-            <<' ';
-    }*/
+        image.seekg(10, ios::cur);
+        image.read(directoryHeader, 2);
+        offsetDirectoryHeader = ((unsigned char)directoryHeader[1]<<8)|
+                                ((unsigned char)directoryHeader[0]);
+    }
+    image.seekg(6, ios::cur);
+    image.read(offsetBytes, 4);
+    offset = ((unsigned char)offsetBytes[3]<<24)|
+             ((unsigned char)offsetBytes[2]<<16)|
+             ((unsigned char)offsetBytes[1]<<8)|
+             ((unsigned char)offsetBytes[0]);
+    uint32_t stripOffsets = offset;
+
+    image.read(directoryHeader, 2);
+    offsetDirectoryHeader = ((unsigned char)directoryHeader[1]<<8)|
+                            ((unsigned char)directoryHeader[0]);
+    image.seekg(SubIFD + 2, ios::beg);
+
+    image.read(directoryHeader, 2);
+    offsetDirectoryHeader = ((unsigned char)directoryHeader[1]<<8)|
+                            ((unsigned char)directoryHeader[0]);
+
+    while(offsetDirectoryHeader!=0x0117)
+    {
+        image.seekg(10, ios::cur);
+        image.read(directoryHeader, 2);
+        offsetDirectoryHeader = ((unsigned char)directoryHeader[1]<<8)|
+                                ((unsigned char)directoryHeader[0]);
+    }
+
+    image.seekg(6, ios::cur);
+    image.read(offsetBytes, 4);
+    offset = ((unsigned char)offsetBytes[3]<<24)|
+             ((unsigned char)offsetBytes[2]<<16)|
+             ((unsigned char)offsetBytes[1]<<8)|
+             ((unsigned char)offsetBytes[0]);
+    uint32_t stripByteCounts = offset;
+
+    cout<<stripByteCounts;
 }
